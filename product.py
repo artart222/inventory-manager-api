98985dd99e3a048251dc619af4c4a68765d9020b
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-09-11 14:49:31 +0330

    Class structure has been changed

--- product.py
+++ product.py
@@ -1,11 +1,12 @@
 from abc import ABC
-from datetime import datetime
+# TODO: Maybe implement time
+# from datetime import datetime
 
 
 class ProductBase:
     def __init__(self) -> None:
+        self.product_id: int | str = 0
         self.name: str = ""
-        self.id: str | int = ""
         self.description: str = ""
         self.price: float = 0.0
         self.quantity: int = 0
@@ -15,7 +16,30 @@ class Product(ABC, ProductBase):
     def __init__(self) -> None:
         ProductBase.__init__(self)
         ABC.__init__(self)
-        self.created_at: datetime
-        self.last_modified_at: datetime
-        self.writen_by: str = ""
-        self.last_modified_by: str = ""
+        # TODO: Maybe implement time.
+        # self.created_at: datetime
+        # self.last_modified_at: datetime
+        # TODO: Maybe implement user.
+        # self.writen_by: str = ""
+        # self.last_modified_by: str = ""
+
+    def set_product_details(self, name, product_id, description, price, quantity):
+        if name is not None:
+            self.name = name
+        if product_id is not None:
+            self.product_id = product_id
+        if description is not None:
+            self.description = description
+        if price is not None:
+            self.price = price
+        if quantity is not None:
+            self.quantity = quantity
+
+    def get_product_details(self):
+        return {
+            "product_id": self.product_id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "quantity": self.quantity,
+        }
