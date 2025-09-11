183fd67af31628d36bc8e954ba26a274cc6d66d3
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-09-11 14:50:19 +0330

    new methods have been used and tested

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 from product import Product
+from db import DataBase
 
 a = Product()
+a.set_product_details(
+    product_id=2411323,
+    name="Car",
+    description="My beatiful car",
+    price=32000,
+    quantity=15,
+)
+print(
+    a.get_product_details().get("product_id"),
+    a.get_product_details().get("name"),
+    a.get_product_details().get("description"),
+    a.get_product_details().get("price"),
+    a.get_product_details().get("quantity"),
+)
+
+
+# d=DataBase.connect_to_db()
+d = DataBase()
+print(
+    d.add_product(
+        a.get_product_details().get("product_id"),
+        a.get_product_details().get("name"),
+        a.get_product_details().get("description"),
+        a.get_product_details().get("price"),
+        a.get_product_details().get("quantity"),
+    )
+)
+print(d.get_product(product_id=2411323))
+del d
+# d.read_config("db.ini", "postgresql")
