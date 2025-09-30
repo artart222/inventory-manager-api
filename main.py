419d55d7164252f48cf1e9a06b33516d52428bf7
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-09-30 16:46:23 +0330

    A full main.py has been written and all db.py and products.py features have been tested

--- main.py
+++ main.py
@@ -3,32 +3,34 @@ from db import DataBase
 
 a = Product()
 a.set_product_details(
-    product_id=2411323,
     name="Car",
-    description="My beatiful car",
+    description="My beautiful car",
     price=32000,
     quantity=15,
 )
-print(
-    a.get_product_details().get("product_id"),
-    a.get_product_details().get("name"),
-    a.get_product_details().get("description"),
-    a.get_product_details().get("price"),
-    a.get_product_details().get("quantity"),
+
+b = Product(
+    name="Cat",
+    description="Out Inventory Cat",
+    price=100,
+    quantity=5
 )
+print(b.get_product_details())
 
+a.set_product_details(quantity=10)
+print(a.get_product_details())
 
-# d=DataBase.connect_to_db()
 d = DataBase()
-print(
-    d.add_product(
-        a.get_product_details().get("product_id"),
-        a.get_product_details().get("name"),
-        a.get_product_details().get("description"),
-        a.get_product_details().get("price"),
-        a.get_product_details().get("quantity"),
-    )
-)
-print(d.get_product(product_id=2411323))
-del d
-# d.read_config("db.ini", "postgresql")
+d.add_product(**a.get_product_details())
+d.add_product(**b.get_product_details())
+
+for i in d.list_products():
+    print(i.get_product_details())
+
+print(type(d.get_product(d.list_products()[0].get_product_details().get("product_id"))))
+print(d.update_product(product_id=d.list_products()[0].get_product_details().get("product_id"),args={"price":10}))
+print(d.delete_product(d.list_products()[0].get_product_details().get("product_id")))
+print(d.list_products()[0].get_product_details())
+
+for i in d.list_products():
+    print(i.get_product_details())
